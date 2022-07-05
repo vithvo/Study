22aacfaebab8400fb0d33170855876f7aa57767b
Хворых Виталий <vithvo@bk.ru>
2022-07-05 12:53:10 +0700

    Refresh

--- app.js
+++ app.js
@@ -27,16 +27,29 @@
 //     alert("We hope that this page looks good");
 // }
 
-let browser = "Chrome";
-if (browser == "Edge") {
-  alert("You've got the Edge");
-} else if (
-  browser == "Chrome" ||
-  browser == "Firefox" ||
-  browser == "Safari" ||
-  browser == "Opera"
-) {
-  alert("Okay we support these browsers too");
-} else {
-  alert("We hope that this page looks good");
+// let browser = "Chrome";
+// if (browser == "Edge") {
+//   alert("You've got the Edge");
+// } else if (
+//   browser == "Chrome" ||
+//   browser == "Firefox" ||
+//   browser == "Safari" ||
+//   browser == "Opera"
+// ) {
+//   alert("Okay we support these browsers too");
+// } else {
+//   alert("We hope that this page looks good");
+// }
+
+const number = +prompt("Введите число между 0 и 3", "");
+
+switch (number) {
+  case 0:
+    alert("Вы ввели число 0");
+    break;
+  case 1:
+    alert("Вы в вели число 1");
+    break;
+  default:
+    alert("Вы ввели число 2, а может 3");
 }
