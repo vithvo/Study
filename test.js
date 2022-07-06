f03e0b03f7497d41d23f8e64334d77e1bb5a7587
Хворых Виталий <vithvo@bk.ru>
2022-07-06 09:05:56 +0700

    Added some tests Pow function'

--- test.js
+++ test.js
@@ -1,9 +1,38 @@
 describe("pow", function () {
-  it("2 в степени 3 будет 8", function () {
-    assert.equal(pow(2, 3), 8);
-  });
+  function makeTest(x) {
+    let expected = x * x * x;
+    it(`${x} в степени 3 будет ${expected}`, function () {
+      assert.equal(pow(x, 3), expected);
+    });
+  }
 
-  it("3 в степени 3 будет 27", function () {
-    assert.equal(pow(3, 3), 27);
+  for (let x = 1; x <= 5; x++) {
+    makeTest(x);
+  }
+});
+
+describe("pow", function () {
+  before(() => alert("Тестирование началось – перед тестами"));
+
+  describe("возводит x в степень 3", function () {
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} в степени 3 будет ${expected}`, function () {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+
+    for (let x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
   });
+  after(() => alert("Тестирование закончилось – после всех тестов"));
+});
+
+describe("тест", function () {
+  beforeEach(() => alert("Перед тестом – начинаем выполнять тест"));
+  afterEach(() => alert("После теста – заканчиваем выполнение теста"));
+
+  it("тест 1", () => alert(1));
+  it("тест 2", () => alert(2));
 });
