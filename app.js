31a4fe4083b596de17dbb89fce0866ebdf92b981
Хворых Виталий <vithvo@bk.ru>
2022-07-11 16:53:52 +0700

    matrix

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 // }
 // let whiteRabbit = { type: "Белый", speak };
 // let hungryRabbit = { type: "Голодный", speak };
-// whiteRabbit.speak("Ах вы ушки-усики мои!" + "Как я оnаздываю!");
+// whiteRabbit.speak("Аx вы yшки-yсики мои!" + "Как я оnаздываю!");
 
-// hungryRabbit.speak("Я бы съел сейчас морковку.");
+// hungryRabbit.speak("Я бы съел сейчас морковкy.");
 
 // speak.call(hungryRabbit, "Я объелся!");
 
@@ -73,7 +73,7 @@
 //     console.log(`${this.type} кролик говорит: '${line}'`);
 //   }
 // }
-// let killerRabbit = new Rabbit("Убийственный");
+// let killerRabbit = new Rabbit("yбийственный");
 // let blackRabbit = new Rabbit("Черный");
 
 // console.log(killerRabbit);
@@ -95,29 +95,66 @@
 // console.log(String(blackRabbit));
 
 class Matrix {
-  constructor(width, height, element = (х, у) => undefined) {
+  constructor(width, height, element = (x, y) => undefined) {
     this.width = width;
     this.height = height;
     this.content = [];
 
-    for (let у = 0; у < height; у++) {
-      for (let х = 0; х < width; х++) {
-        this.content[y * width + х] = element(x, у);
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.content[y * width + x] = element(x, y);
       }
     }
   }
-  get(x, у) {
-    return this.content[y * this.width + х];
+  get(x, y) {
+    return this.content[y * this.width + x];
   }
-  set(x, у, value) {
-    this.content[y * this.width + х] = value;
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
   }
 }
-// get(x, у) {
-// return this.content[y * this.width + х];
-// }
 
-// set(x, у, value) {
-// this.content[y * this.width + х] value;
-// }
-// }
+class Matrixiterator {
+  constructor(matrix) {
+    this.x = 0;
+
+    this.y = 0;
+    this.matrix = matrix;
+  }
+  next() {
+    if (this.y == this.matrix.height) return { done: true };
+    let value = {
+      x: this.x,
+      y: this.y,
+      value: this.matrix.get(this.x, this.y),
+    };
+    this.x++;
+    if (this.x == this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+    return { value, done: false };
+  }
+}
+
+Matrix.prototype[Symbol.iterator] = function () {
+  return new Matrixiterator(this);
+};
+
+class SymmetricMatrix extends Matrix {
+  constructor(size, element = (x, y) => undefined) {
+    super(size, size, (x, y) => {
+      if (x < y) return element(y, x);
+      else return element(x, y);
+    });
+  }
+
+  set(x, y, value) {
+    super.set(x, y, value);
+    if (x != y) {
+      super.set(y, x, value);
+    }
+  }
+}
+let matrix = new SymmetricMatrix(5, (x, y) => `${x},${y}`);
+console.log(matrix.get(2, 3));
