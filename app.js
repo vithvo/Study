3b8292e91e5d6119bc5a8a19a22b628453513db8
Хворых Виталий <vithvo@bk.ru>
2022-07-12 11:08:49 +0700

    finish exersices

--- app.js
+++ app.js
@@ -186,27 +186,96 @@
 
 // ======================================================================
 
-class Vec {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
+// class Vec {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
 
-  plus(other) {
-    return new Vec(this.x + other.x, this.y + other.y);
-  }
+//   plus(other) {
+//     return new Vec(this.x + other.x, this.y + other.y);
+//   }
 
-  minus(other) {
-    return new Vec(this.x - other.x, this.y - other.y);
-  }
+//   minus(other) {
+//     return new Vec(this.x - other.x, this.y - other.y);
+//   }
 
-  get length() {
-    return Math.sqrt(this.x * this.x + this.y * this.y);
-  }
-}
+//   get length() {
+//     return Math.sqrt(this.x * this.x + this.y * this.y);
+//   }
+// }
 
-let vec = new Vec(2, 3);
-console.log(vec);
+// let vec = new Vec(2, 3);
+// console.log(vec);
 
-console.log(new Vec(1, 2).minus(new Vec(2, 3)));
-console.log(new Vec(3, 4).length);
+// console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// console.log(new Vec(3, 4).length);
+
+// ======================================================================
+
+// class Group {
+//   constructor() {
+//     this.members = [];
+//   }
+
+//   add(value) {
+//     if (!this.has(value)) {
+//       this.members.push(value);
+//     }
+//   }
+
+//   delete(value) {
+//     this.members = this.members.filter((v) => v !== value);
+//   }
+
+//   has(value) {
+//     return this.members.includes(value);
+//   }
+
+//   static from(collection) {
+//     let group = new Group();
+//     for (let value of collection) {
+//       group.add(value);
+//     }
+//     return group;
+//   }
+//   [Symbol.iterator]() {
+//     return new GroupIterator(this);
+//   }
+// }
+
+// // let group = Group.from([10, 20]);
+// // console.log(group);
+// // console.log(group.has(10));
+
+// // console.log(group.has(30));
+
+// // group.add(10);
+// // group.delete(10);
+// // console.log(group.has(10));
+
+// class GroupIterator {
+//   constructor(group) {
+//     this.group = group;
+//     this.position = 0;
+//   }
+
+//   next() {
+//     if (this.position >= this.group.members.length) {
+//       return { done: true };
+//     } else {
+//       let result = { value: this.group.members[this.position], done: false };
+//       this.position++;
+//       return result;
+//     }
+//   }
+// }
+
+// for (let value of Group.from(["a", "b", "c"])) {
+//   console.log(value);
+// }
+// ======================================================================
+
+let map = { one: true, two: true, hasOwnProperty: true };
+
+console.log(Object.prototype.hasOwnProperty.call(map, "one"));
